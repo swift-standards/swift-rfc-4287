5ff53679f1b517528e0f63d5d60ca95c31c6701b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:04:01 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -2,18 +2,6 @@
 
 import PackageDescription
 
-extension String {
-    static let rfc4287: Self = "RFC 4287"
-}
-
-extension Target.Dependency {
-    static var rfc4287: Self { .target(name: .rfc4287) }
-    static var rfc2822: Self { .product(name: "RFC 2822", package: "swift-rfc-2822") }
-    static var rfc3339: Self { .product(name: "RFC 3339", package: "swift-rfc-3339") }
-    static var rfc3987: Self { .product(name: "RFC 3987", package: "swift-rfc-3987") }
-    static var rfc4648: Self { .product(name: "RFC 4648", package: "swift-rfc-4648") }
-}
-
 let package = Package(
     name: "swift-rfc-4287",
     platforms: [
@@ -37,22 +25,17 @@ let package = Package(
     targets: [
         .target(
             name: "RFC 4287",
-            dependencies: [.rfc2822, .rfc3339, .rfc3987, .rfc4648]
+            dependencies: [.product(name: "RFC 2822", package: "swift-rfc-2822"), .product(name: "RFC 3339", package: "swift-rfc-3339"), .product(name: "RFC 3987", package: "swift-rfc-3987"), .product(name: "RFC 4648", package: "swift-rfc-4648")]
         ),
         .testTarget(
             name: "RFC 4287 Tests",
             dependencies: [
-                "RFC 4287"
+                .target(name: "RFC 4287")
             ]
         ),
     ],
     swiftLanguageModes: [.v6]
 )
-
-extension String {
-    var tests: Self { self + " Tests" }
-    var foundation: Self { self + " Foundation" }
-}
 
 for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
     let ecosystem: [SwiftSetting] = [
